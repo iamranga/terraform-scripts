fcb95aa4de43c350056baefd842d5388210bc3b7
Rangarajan <rangarajan.ramadass@cognizant.com>
2023-05-30 02:07:48 +0530

    edited main.tf

--- aws/sample/main.tf
+++ aws/sample/main.tf
@@ -2,13 +2,14 @@ terraform {
   required_providers {
     aws = {
       source =  "hashicorp/aws"
-      version = "3.7.1"
+      version = "4.0.0"
     }
   }
 }
 
 provider "aws" {
   region = "us-east-1"
+  shared_credentials_files = ["tfvars/profile/cloud/ADM-AWS/variables"]
 }
 
 resource "aws_instance" "ec2_instance" {
