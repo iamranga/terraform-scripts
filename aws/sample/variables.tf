cc120d84d5a6354a5bea7321b23d980fa793d293
Rangarajan <rangarajan.ramadass@cognizant.com>
2023-05-30 04:09:14 +0530

    added access key and secret key variables

--- aws/sample/variables.tf
+++ aws/sample/variables.tf
@@ -26,16 +26,3 @@ variable "number_of_instances" {
 variable "ami_key_pair_name" {
   default = "morpheus-key"
 }
-
-variable "access_key" {
-  type = "string"
-}
-
-variable "secret_key" {
-  type = "string"
-}
-
-module "tfvars" {
-  access_key = var.access_key
-  secret_key = var.secret_key
-}
