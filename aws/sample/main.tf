33392fd97f5dc6aef6cb73a926d39ca2dfdb11a5
Rangarajan <rangarajan.ramadass@cognizant.com>
2023-05-30 00:23:05 +0530

    added closing braces

--- aws/sample/main.tf
+++ aws/sample/main.tf
@@ -51,11 +51,11 @@ resource "aws_security_group" "project-iac-sg" {
 
 
 resource "aws_instance" "project-iac" {
-  ami = lookup(var.awsprops, "ami")
-  instance_type = lookup(var.awsprops, "itype")
-  subnet_id = lookup(var.awsprops, "subnet") #FFXsubnet2
+  ami                         = lookup(var.awsprops, "ami")
+  instance_type               = lookup(var.awsprops, "itype")
+  subnet_id                   = lookup(var.awsprops, "subnet") #FFXsubnet2
   associate_public_ip_address = lookup(var.awsprops, "publicip")
-  key_name = lookup(var.awsprops, "keyname")
+  key_name                    = lookup(var.awsprops, "keyname")
 
 
   vpc_security_group_ids = [
@@ -63,13 +63,14 @@ resource "aws_instance" "project-iac" {
   ]
   root_block_device {
     delete_on_termination = true
-    iops = 150
-    volume_size = 50
-    volume_type = "gp2"
+    iops                  = 150
+    volume_size           = 50
+    volume_type           = "gp2"
   }
   tags = {
-    Name ="SERVER01"
+    Name        = "SERVER01"
     Environment = "DEV"
-    OS = "UBUNTU"
-    Managed = "IAC"
+    OS          = "UBUNTU"
+    Managed     = "IAC"
   }
+}
