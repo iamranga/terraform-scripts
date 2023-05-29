8c4c51e10ab0fbec46b64a33de3b6e1c41dfc0c6
Rangarajan <rangarajan.ramadass@cognizant.com>
2023-05-30 02:00:49 +0530

    added terraform provider aws version section

--- aws/sample/main.tf
+++ aws/sample/main.tf
@@ -1,7 +1,14 @@
+terraform {
+  required_providers {
+    aws = {
+      source =  "hashicorp/aws"
+      version = "3.7.1"
+    }
+  }
+}
+
 provider "aws" {
   region = "us-east-1"
-  access_key = ""
-  secret_key = ""
 }
 
 resource "aws_instance" "ec2_instance" {
