3fd0fc8d53fe462e7cc5fa5c641c9d204911e9ef
Rangarajan <rangarajan.ramadass@cognizant.com>
2023-05-30 02:41:13 +0530

    edited main.tf

--- aws/sample/main.tf
+++ aws/sample/main.tf
@@ -1,12 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source =  "hashicorp/aws"
-      version = "4.0.0"
-    }
-  }
-}
-
 provider "aws" {
   region = "us-east-1"
   shared_credentials_files = ["tfvars/profile/cloud/ADM-AWS/variables"]
